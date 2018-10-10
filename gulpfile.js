6c2c086116e2f1a49ef556e5ca25640eb8084d68
Tomasz Gola <tomasz@MacBook-Pro-Tomasz.local>
2018-10-10 21:23:21 +0200

    little cleaning, add autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function(){
+gulp.task('sass', () =>
     gulp.src('./assets/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./assets/css'))
         .pipe(browserSync.reload({
             stream: true
-        }));
-});
+        }))
+);
 
-gulp.task('browser-sync', function(){
+gulp.task('autoprefixer', () =>
+    gulp.src('./assets/css/main.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./assets/css'))
+);
+
+gulp.task('browser-sync', () =>
     browserSync.init({
         server: {
             baseDir: './'
         }
-    });
-});
+    })
+);
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'sass'], () => {
     gulp.watch('./assets/scss/main.scss', ['sass']);
-    gulp.watch('./assets/css/main.css').on('change', browserSync.reload);
+    gulp.watch('./assets/css/main.css', ['autoprefixer']).on('change', browserSync.reload)
 });
